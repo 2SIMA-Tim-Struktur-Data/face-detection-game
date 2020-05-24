ab88aa5a1f930a40a65e78f896517702f1bb315b
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-05-24 15:43:48 +0700

    Add modal image

--- js/historyuser.js
+++ js/historyuser.js
@@ -24,3 +24,13 @@ function callbackFunc() {
  
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
