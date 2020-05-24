17950e636653eb0c1092f8eef23099967269d230
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-05-24 15:43:21 +0700

    Show user history from database with php

--- historyuser.php
+++ historyuser.php
@@ -1,21 +1,23 @@
-<?php session_start() ?>
+<?php
+session_start();
+include_once('php/db_handler.php');
+
+if( isset( $_SESSION['username'])){
+    $thisUser = $_SESSION['username'];
+}   
+$sql_history = "SELECT `created_at`, `score`, `emotion` ,`image_path` FROM `snapshots` WHERE `user_no` = '".$thisUser."' ";
+$result = mysqli_query($conn,$sql_history);
+$resultCheck = mysqli_num_rows($result);
+
+?>
+
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <link rel="apple-touch-icon" type="image/png"
-        href="https://static.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png">
-    <meta name="apple-mobile-web-app-title" content="CodePen">
-    <link rel="shortcut icon" type="image/x-icon"
-        href="https://static.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico">
-    <link rel="mask-icon" type=""
-        href="https://static.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg"
-        color="#111">
-    <link rel="stylesheet" href="css/historyuser.scss">
     <link rel="stylesheet" href="css/historyuser.css">
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="js/historyuser.js">
     <title>History User | Face Detection Game</title>
 </head>
 
@@ -26,54 +28,43 @@
 
     <section class="timeline">
         <ul>
-            <li class="in-view">
-                <div>
-                    <time>21 March 2020</time>
-                    <div class="discovery">
-                        <img src="#" alt="">
-                        <p>
-                            You Are Happy
-                        </p>
-                    </div>
-                    <div class="scientist">
-                        <h1>Point</h1>
-                        <span>95 Point</span>
-                    </div>
-                </div>
-            </li>
-            <li class="in-view">
-                <div>
-                    <time>23 March 2020</time>
-                    <div class="discovery">
-                        <img src="#" alt="">
-                        <p>
-                            You Are Sad
-                        </p>
-                    </div>
-                    <div class="scientist">
-                        <h1>Point</h1>
-                        <span>50 Point</span>
-                    </div>
-                </div>
-            </li>
-            <li class="in-view">
-                <div>
-                    <time>30 March 2020</time>
-                    <div class="discovery">
-                        <img src="#" alt="">
-                        <p>
-                            You Are Not That Happy
-                        </p>
-                    </div>
-                    <div class="scientist">
-                        <h1>Point</h1>
-                        <span>70 Point</span>
-                    </div>
-                </div>
-            </li>
+            <?php 
+                if($resultCheck > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo "  <li class='in-view'>
+                                    <div>
+                                        <time>".$row['created_at']."</time>
+                                        <div class='discovery'>
+                                            <img width='70px' onclick='openModal();' class='myImage' src='img/snapshots/".$row['image_path']."' />
+                                            <p>You Are ".$row['emotion']."</p>
+                                        </div>
+                                        <div class='scientist'>
+                                            <h1>".$row['score']."</h1>
+                                            <span>Point</span>
+                                        </div>
+                                    </div>
+                                </li>";
+                        echo "  <div id='myModal' class='modal'>
+                                    <span class='close cursor' onclick='closeModal()'>&times;</span>
+                                    <div class='modal-content'>
+                                        <img src='img/snapshots/".$row['image_path']."'>
+                                    </div>
+                                </div>
+                        ";
+                        }
+                    }
+                    else {
+                        echo "<div class='box'><h1>No History!</h1> <p>You haven't played a game...</p></div>";
+                    }
+                ?>
+        </ul>
 
+        <?php 
+        
+        ?>
+        
     </section>
-
+    <script src="./js/historyuser.js"></script>
 </body>
 
 </html>
